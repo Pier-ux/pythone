488d3ec40a950ceae5795439d5f82b8591ec5982
Pier-ux <pierangeloquarato94@gmail.com>
2025-02-06 13:48:21 +0100

    Update esempio_02.py

--- esempio_02.py
+++ esempio_02.py
@@ -3,7 +3,7 @@ valore = int(input("Inserisci la lunghezza"))
 lettera = str(input("Inserisci una lettera"))
 contatore = 1
 for i in range (valore):
-    i = (" " * valore + lettera * contatore)
+    i = (" " * (valore - 1) + lettera * contatore)
     valore -= 1
     contatore += 2
     print(i)
